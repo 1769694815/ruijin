5b3265fbda8b55e552f64c6f378d05bf94e15069
徐文 <1769694815@qq.com>
2020-05-21 11:37:54 +0800

    新增indexDB

---
+++ src/dataStore/index.js
@@ -0,0 +1,21 @@
+import IndexDBWrapper from 'indexdbwrapper'
+
+export default {
+  indexedDB: new IndexDBWrapper('ruijin', 1, {
+    onupgradeneeded: e => {
+      const db = e.target.result
+      const objStore = db.createObjectStore('user', {
+        autoIncrement: true,
+        keyPath: 'id'
+      })
+      objStore.createIndex('name', 'name', { unique: !1 })
+      objStore.createIndex('birthday', 'birthday', { unique: !1 })
+      objStore.createIndex('age', 'age', { unique: !1 })
+      objStore.createIndex('category', 'category', { unique: !1 })
+      objStore.createIndex('note', 'note', { unique: !1 })
+    }
+  }),
+  openDB: function () {
+    this.indexedDB.openDB()
+  }
+}
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,19 +3,26 @@ import axios from 'axios'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import './styles/index.scss' // global css
+
 import App from './App'
-import router from './router'
 import store from './store'
+import router from './router'
+import db from '../dataStore/index'
+import './icons'
+import './permission'
+
 Vue.use(Element)
 
+Vue.prototype.$db = db.indexedDB
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  el: '#app',
   router,
   store,
-  template: '<App/>'
-}).$mount('#app')
+  render: h => h(App)
+})
